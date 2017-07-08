2e7f5efb77cc2b5a1fa97a4eb57694e50cf94ae7
akshayanandraut <akshay.raut0303@gmail.com>
2017-07-08 18:20:15 +0530

    added unitconverter api

--- api/unit/index.php
+++ api/unit/index.php
@@ -18,45 +18,45 @@ if(in_array($type,$typex))
 						echo '**_Unit code_** | **_Unit_**\n\n';
 						for($i=0; $i<sizeof($units); $i++)
 {
-	echo '**'.$units[$i].'** | '.$unitFull[i].'\n';
+	echo '**'.$units[$i].'** | '.$unitFull[i].'<br>';
 }
-echo '\n\n';
+echo '<br><br>';
 						break;
 		case "temp": 
 						$units = array('celsius','kelvin','fahrenheit');
 						$baseToFinal = array(1,274.15,33.8);
 						$unitFull=array('celsius','Kelvin','Fahrenheit');
 						$unitToBase = array(1,-272.15,-17.222222222);
-						echo '**_Unit code_** | **_Unit_**\n\n';
+						echo '**_Unit code_** | **_Unit_**<br><br>';
 						for($i=0; $i<sizeof($units); $i++)
 {
-	echo '**'.$units[$i].'** | '.$unitFull[i].'\n';
+	echo '**'.$units[$i].'** | '.$unitFull[i].'<br>';
 }
-echo '\n\n';
+echo '<br><br>';
 						break;
 		case "area": 
 						$units = array('sqmts','sqkmts','sqcmts','sqmmts','sqmicromts','hectare','sqmile','sqyard','sqft','sqinch','acre');
 						$unitFull=array('Square Meters','Square Kilometers','Square Centimeters','Square Millimeters','Square Micrometers','Hectares','Square Mile','Square Yards','Square Feet','Square Inches','Acres');
 						$baseToFinal = array(1,0.000001,10000,1000000,1000000000000,0.0001,3.495337156393730,1.1959900463,10.763910417,1550.0031,0.0002471054);
 						$unitToBase = array(1,1000000,0.0001,0.000001,-9.28171817154095,10000,2589990,0.83612736,0.09290304,0.00064516,4046.8564224);
-						echo '**_Unit code_** | **_Unit_**\n\n';
+						echo '**_Unit code_** | **_Unit_**<br><br>';
 						for($i=0; $i<sizeof($units); $i++)
 {
-	echo '**'.$units[$i].'** | '.$unitFull[i].'\n';
+	echo '**'.$units[$i].'** | '.$unitFull[i].'<br>';
 }
-echo '\n\n';
+echo '<br><br>';
 						break;
 		case "vol": 
 						$units = array('cumts','cukmts','cucmts','cummts','lts','mlts','usgallon','usquart','uspint','uscup','usfluidounce','ustablebspoon','usteaspoon','impgallon','impquart','imppint','impfluidounce','imptablespoon','impteaspoon','cumile','cuyard','cufoot','cuinch');
 						$unitFull=array('Cubic Meter','Cubic Kilometer','Cubic Centimeter','Cubic Millimeter','Liter','Millimeter','US Gallon','US Quart','US Pint','US Cup','US Fluid Ounce','US Table Spoon','US Tea Spoon','Imperial Gallon','Imperial Quart','Imperial Pint','Imperial Fluid Ounce','Imperial Table Spoon','Imperial Tea Spoon','Cubic Mile','cubic Yard','Cubic Foot','Cubic Inch');
 						$baseToFinal = array(1,-6.28171817154095,1000000,1000000000,1000,1000000,264.17217686,1056.6887074,2113.3774149,4226.7548297,33814.038638,67628.077276,202884.23183,219.9692483,879.8769932,1759.7539864,35195.079728,56312.127565,168936.38269,-3.47849222462546,1.3079506193,35.314666721,61023.744095);
 						$unitToBase = array(1,1000000000,0.000001,-6.28171817154095,0.001,0.000001,0.00378541,0.0009463525, 0.0004731763,0.0002365881,0.0000295735,0.0000147868,0.0000049289,0.00454609,0.0011365225,0.00005682613,0.0000284131,0.0000177582,0.0000059194,4168180000,0.764554858,0.0283168466,0.0000163871);
-						echo '**_Unit code_** | **_Unit_**\n\n';
+						echo '**_Unit code_** | **_Unit_**<br><br>';
 						for($i=0; $i<sizeof($units); $i++)
 {
-	echo '**'.$units[$i].'** | '.$unitFull[i].'\n';
+	echo '**'.$units[$i].'** | '.$unitFull[i].'<br>';
 }
-echo '\n\n';
+echo '<br><br>';
 						break;
 		case "weight": 
 						$units = array('kgms','gms','mgms','metricton','longton','shortton','pound','ounce','carrat','atomicmass');
@@ -64,24 +64,24 @@ echo '\n\n';
 						$baseToFinal = array(1,1000,1000000,0.001,0.0009842073,0.0011023122,2.2046244202,35.273990723,5000,42.36986462962879);
 						
 						$unitToBase = array(1,0.001,0.000001,1000,1016.04608,907.184,0.453592,0.0283495,0.0002,-22.4861837516325);
-						echo '**_Unit code_** | **_Unit_**\n\n';
+						echo '**_Unit code_** | **_Unit_**<br><br>';
 						for($i=0; $i<sizeof($units); $i++)
 {
-	echo '**'.$units[$i].'** | '.$unitFull[i].'\n';
+	echo '**'.$units[$i].'** | '.$unitFull[i].'<br>';
 }
-echo '\n\n';
+echo '<br><br>';
 						break;
 		case "time":
 						$units = array('sec','milisec','microsec','nanosec','picosec','min','hour','day','week','month','year');
 						$unitFull=array('Second','Millisecond','Microsecond','Nanosecond','Picosecond','Minute','Hour','Day','Week','Month','Year');
 						$baseToFinal = array(1,1000,1000000,1000000000,1000000000000,0.0166666667,0.0002777778,0.0000115741,0.0000016534,3.336458392160853,0.613715327253758);
 						$unitToBase = array(1,0.001,0.000001,-6.28171817154095,-9.28171817154095,60,3600,86400,604800,2629800,3155760);
-						echo '**_Unit code_** | **_Unit_**\n\n';
+						echo '**_Unit code_** | **_Unit_**<br><br>';
 						for($i=0; $i<sizeof($units); $i++)
 {
-	echo '**'.$units[$i].'** | '.$unitFull[i].'\n';
+	echo '**'.$units[$i].'** | '.$unitFull[i].'<br>';
 }
-echo '\n\n';
+echo '<br><br>';
 						break;
 	}
 	if (in_array($from, $units) && in_array($from, $units) && $from != $to)
