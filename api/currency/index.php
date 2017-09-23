d6bb42c476b7db0ff9194bc67f923e0e46cb8032
Urvi Paithankar <--global>
2017-09-23 20:43:50 +0530

    currency trial

--- api/currency/index.php
+++ api/currency/index.php
@@ -1,16 +1,20 @@
+<html>
+	<head>
+<!-- Global Site Tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-106918659-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)};
+  gtag('js', new Date());
+
+  gtag('config', 'UA-106918659-1');
+</script>
+</head>
+<body>
 <?php 
 header('Access-Control-Allow-Origin: *');  
 header('Access-Control-Allow-Headers: *');  
 header('Access-Control-Allow-Methods: *');  
-echo "<script>
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-			ga('create', 'UA-106918659-1', 'auto');
-			ga('send', 'pageview');
-		</script>";
 $error = "NO_ERROR";
 $error_code = "000";
 $error_desc = "NO ERROR.";
@@ -107,3 +111,5 @@ else
 					echo $output;
 }	
 ?>
+</body>
+</html>
